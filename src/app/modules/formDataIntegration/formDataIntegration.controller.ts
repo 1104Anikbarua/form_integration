40a4075e856aed7bb7b907b0ad0d0f583a99ad2b
Anik Barua <anikbb05@gmail.com>
2024-09-12 17:10:54 +0600

    test

--- src/app/modules/formDataIntegration/formDataIntegration.controller.ts
+++ src/app/modules/formDataIntegration/formDataIntegration.controller.ts
@@ -4,8 +4,20 @@ import { handleAsyncTryCatch } from '../../utlis/tryCatch.utlis';
 
 // add form
 const addForm = handleAsyncTryCatch(async (req, res) => {
-  console.log(req.body);
-  const result = await formServices.addForm(req.body);
+  // console.log(req.body);
+  const formData = req.body;
+  if (!formData || !formData.answer || !formData.answer.answers) {
+    throw new Error('Missing required form data');
+  }
+  // Process the form data (example)
+  const answers = formData.answer.answers;
+  // answers.forEach((answer) => {
+  //   console.log(`Question ID: ${answer.q}, Answer: ${answer.t || answer.c}`);
+  // });
+  const formName = answers[0].t;
+  // const isActive=answers[1].
+  console.log(formName);
+  const result = await formServices.addForm(formName);
   handleSendResposne(res, {
     statusCode: httpStatus.OK,
     success: true,
