676845bbc68effe47abce23621fb94864d3860a8
Anik Barua <anikbb05@gmail.com>
2024-09-12 18:03:28 +0600

    test

--- src/app/modules/formDataIntegration/formDataIntegration.service.ts
+++ src/app/modules/formDataIntegration/formDataIntegration.service.ts
@@ -2,6 +2,7 @@ const addForm = async (payload: any) => {
   const answers = payload;
   const formName = answers[0]?.t;
   const isActive = answers[1]?.c[1]?.t;
+  console.log(formName, isActive);
   return { formName, isActive };
 };
 const formServices = {
